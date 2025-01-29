959a6ef91c2a467663a612d7c4589ff178c9f39e
presentY1897 <presentY1897@gmail.com>
2025-01-29 15:16:33 +0900

    Fix: Log in comp

--- app/components/log-in.tsx
+++ app/components/log-in.tsx
@@ -1,11 +1,13 @@
+import AnimatePlaceholderInput from "./atom/animate-placeholder-input";
 import LabelInput from "./atom/label-input";
 
 
 export default function LogIn() {
 	return (
 		<div className="flex flex-col gap-2">
-			<LabelInput label="Username" type="email" placeholder="username or email"/>
+			<AnimatePlaceholderInput type="email" placeholder="username or email"/>
 			<LabelInput label="Password" type="password" placeholder=""/>
+			<a href="/pages/find-password" className="text-sm text-blue-500">Forgot your password?</a>
 		</div>
 	)
 }
