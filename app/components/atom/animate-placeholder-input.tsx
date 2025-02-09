37f2f27f83cf1717dae798de845decd8dc5842fe
presentY1897 <presentY1897@gmail.com>
2025-02-09 11:26:41 +0900

    fix: css

--- app/components/atom/animate-placeholder-input.tsx
+++ app/components/atom/animate-placeholder-input.tsx
@@ -3,9 +3,9 @@
 export default function AnimatePlaceholderInput( { type, placeholder }: { type: string, placeholder: string }) {
 	return (
 		<div className="flex flex-col">
-			<div className="peer-wrap flex flex-col bg-black rounded-md p-2 focus-within:ring-2 focus-within:ring-white">
-				<input className="peer place-content-center rounded-md bg-black text-gray-700 focus:outline-none" type={type}/>
-				<div className="p-1 absolute justify-items-center align-middle text-center place-content-center pointer-events-none select-none bg-black text-gray-500 peer-focus:transition peer-focus:duration-200 peer-focus:-translate-x-1 peer-focus:-translate-y-full">{placeholder}</div>
+			<div className="flex items-center bg-black rounded-md p-2 ring-2 ring-white focus-within:ring-white">
+				<input className="peer rounded-md bg-black text-gray-700 focus:outline-none" type={type}/>
+				<div className="p-1 absolute justify-items-center align-middle text-center place-content-center pointer-events-none select-none bg-black text-gray-500 peer-focus:transition peer-focus:duration-200 peer-focus:-translate-x-1 peer-focus:-translate-y-3/4">{placeholder}</div>
 			</div>
 		</div>
 	)
