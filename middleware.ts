82ce9b1960f29319307c8b605c06009f72f90667
presentY1897 <presentY1897@gmail.com>
2025-02-22 10:27:32 +0900

    Add Routing

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,9 @@
-import { NextRequest, NextResponse } from "next/server";
+import createMiddleware from 'next-intl/middleware';
+import { routing } from './i18n/routing';
 
-const locales = ['en-US', 'ko']
-
-// Get the preferred locale, similar to the above or using a library
-function getLocale() {
-	return 'en-US/'
-}
-
-export function middleware(request: NextRequest) {
-	// Check if there is any supported locale in the pathname
-	const { pathname } = request.nextUrl
-	const pathnameHasLocale = locales.some(
-		(locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
-	)
-	if (pathnameHasLocale) return
-
-	// Redirect if there is no locale
-	const locale = getLocale()
-	request.nextUrl.pathname = `/${locale}${pathname}`
-	// e.g. incoming request is /products
-	// The new URL is now /en-US/products
-	return NextResponse.redirect(request.nextUrl)
-}
+export default createMiddleware(routing);
 
 export const config = {
-	matcher: [
-		'/((?!_next).*)',
-	],
-}
+	// Match only internationalized pathnames
+	matcher: ['/', '/(en|ko)/:path*']
+};
