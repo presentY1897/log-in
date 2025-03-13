0dc191e00bc40165b560b2137522558b12512033
presentY1897 <presenty1897@gmail.com>
2025-03-13 12:00:09 +0900

    feat: add vercel bypass key

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,6 +28,9 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://127.0.0.1:3000',
 
+		extraHTTPHeaders: {
+      'x-vercel-protection-bypass': process.env.VERCEL_AUTOMATION_BYPASS_SECRET || '',
+		},
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
