49a86676116d19f65fff7ab15ebba6315c7ace3d
presentY1897 <presenty1897@gmail.com>
2025-03-18 17:05:01 +0900

    feat: implements storybook addon themes

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,18 @@
 import type { Preview } from '@storybook/react'
+import { withThemeByDataAttribute } from '@storybook/addon-themes';
 import '../app/globals.css'
 
 const preview: Preview = {
+	decorators: [
+		withThemeByDataAttribute({
+			themes: {
+				light: 'light',
+				dark: 'dark',
+			},
+			defaultTheme: 'light',
+			attributeName: 'class',
+		})
+	],
 	parameters: {
 		controls: {
 			matchers: {
