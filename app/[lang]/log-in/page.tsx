0ac52047f6328dd27f30c2f40b548e6b489b73b4
presentY1897 <presenty1897@gmail.com>
2025-03-24 15:46:47 +0900

    fix: animate placeholder dir

--- app/[lang]/log-in/page.tsx
+++ app/[lang]/log-in/page.tsx
@@ -1,12 +1,24 @@
-import AnimatePlaceholderInput from "@/app/components/atoms/animate-placeholder-input";
+"use client";
+
+import AnimatePlaceholderInput from "@/components/atoms/animate-placeholder-input";
+import { useState } from "react";
 
 export default function Home() {
-	return (
-		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-			<div className="flex flex-col gap-2 row-start-2 items-center sm:items-start">
-				<AnimatePlaceholderInput type="email" placeholder="Username or email" />
-				<button className="bg-blue-500 text-white rounded-md p-2">Log In</button>
-			</div>
-		</div>
-	)
+  const [username, setUsername] = useState("");
+  const usernameChanged = (value: string) => setUsername(value);
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="flex flex-col gap-2 row-start-2 items-center sm:items-start">
+        <AnimatePlaceholderInput
+          type="email"
+          placeholder="Username or email"
+          inputValue={username}
+          inputValueChange={usernameChanged}
+        />
+        <button className="bg-blue-500 text-white rounded-md p-2">
+          Log In
+        </button>
+      </div>
+    </div>
+  );
 }
