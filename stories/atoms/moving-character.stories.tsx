30e0eda6010f2f293444782bc8e4d4d864193afa
presentY1897 <presenty1897@gmail.com>
2025-03-25 16:48:29 +0900

    feat: create charcter moving and spinning

---
+++ stories/atoms/moving-character.stories.tsx
@@ -0,0 +1,34 @@
+import MovingCharacter from "@/components/atoms/moving-character";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Atoms/MovingCharacter",
+  component: MovingCharacter,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof MovingCharacter>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    char: "A",
+    movementSpeed: 10,
+    rotationSpeed: 10,
+    initialPosition: { x: 0, y: 0 },
+    getNextPosition: () => ({
+      x: Math.random() * 100,
+      y: Math.random() * 100,
+    }),
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ width: "100px", height: "100px" }}>
+        <Story />
+      </div>
+    ),
+  ],
+};
