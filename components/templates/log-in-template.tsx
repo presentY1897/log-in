f105f4263b68afff95cfb87363889aee0d4622bb
presentY1897 <presenty1897@gmail.com>
2025-03-25 15:50:47 +0900

    fix: add border line to log in template

--- components/templates/log-in-template.tsx
+++ components/templates/log-in-template.tsx
@@ -8,9 +8,11 @@ export default function LogInTemplate({
   innerContent,
 }: LogInTemplateProps) {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-background gap-5">
-      <div className="text-4xl font-bold text-primary">{titleText}</div>
-      {innerContent}
+    <div className="flex items-center justify-center h-screen bg-background">
+      <div className="flex flex-col items-center justify-center gap-5 p-7 rounded-lg border-2 border-primary">
+        <div className="text-4xl font-bold text-primary">{titleText}</div>
+        {innerContent}
+      </div>
     </div>
   );
 }
