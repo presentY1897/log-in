d60065a8610b21ccca367a1f23432c12e065438e
presentY1897 <presenty1897@gmail.com>
2025-03-26 11:29:59 +0900

    fix: use translate

--- components/pages/log-in-password-page.tsx
+++ components/pages/log-in-password-page.tsx
@@ -1,15 +1,17 @@
-import React from "react";
 import LogInTemplate from "../templates/log-in-template";
 import LogInPassword from "../organisms/log-in-password";
+import { useTranslations } from "next-intl";
 
 interface LogInPasswordProps {
   username: string;
 }
 
 export default function LogInPasswordPage({ username }: LogInPasswordProps) {
+  const translate = useTranslations("LogIn");
+
   return (
     <LogInTemplate
-      titleText="Log In"
+      titleText={translate("Title")}
       innerContent={<LogInPassword username={username} />}
     />
   );
