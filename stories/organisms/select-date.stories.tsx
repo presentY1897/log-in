012fe8b93cf8970064ef877ab03870dade1d0a4f
presentY1897 <presenty1897@gmail.com>
2025-03-26 17:38:30 +0900

    fix: fix right coords

--- stories/organisms/select-date.stories.tsx
+++ stories/organisms/select-date.stories.tsx
@@ -17,43 +17,45 @@ export const Default: Story = {
   args: {
     startDate: new Date(1900, 0, 1),
   },
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    const clock = canvas.getByTestId("clockHandler");
-    const clockDate = canvas.getByTestId("clockDate");
-
-    const rect = clock.getBoundingClientRect();
-    console.log(rect);
-    const centerX = rect.left + rect.width / 2;
-    const centerY = rect.top + rect.height / 2;
-
-    await userEvent.pointer({
-      keys: "[MouseLeft]",
-      target: clock,
-      coords: { x: centerX, y: centerY },
-    });
-    expect(clockDate).toHaveTextContent("1931-04-24");
-
-    await userEvent.pointer({
-      keys: "[MouseLeft]",
-      target: clock,
-      coords: { x: centerY - 1, y: centerY },
-    });
-    expect(clockDate).toHaveTextContent("1993-12-04");
-
-    await userEvent.pointer({
-      keys: "[MouseLeft]",
-      target: clock,
-      coords: { x: Math.floor(centerX), y: Math.floor(centerY) },
-    });
-    expect(clockDate).toHaveTextContent("1900-01-01");
-  },
 };
 
 export const DarkTheme: Story = {
   ...Default,
   globals: {
     theme: "dark",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const clock = canvas.getByTestId("clockHandler");
+    const clockDate = canvas.getByTestId("clockDate");
+
+    const rect = clock.getBoundingClientRect();
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
+
+    await userEvent.pointer({
+      keys: "[MouseLeft]",
+      target: clock,
+      coords: { x: centerX + 5, y: centerY },
+    });
+    expect(clockDate).toHaveTextContent("1931-04-24");
+
+    await userEvent.pointer({
+      keys: "[MouseLeft]",
+      target: clock,
+      coords: { x: centerX - 5, y: centerY },
+    });
+    expect(clockDate).toHaveTextContent("1993-12-04");
+
+    await userEvent.pointer({
+      keys: "[MouseLeft]",
+      target: clock,
+      coords: {
+        x: centerX,
+        y: Math.floor(centerY) - rect.height / 2,
+      },
+    });
+    expect(clockDate).toHaveTextContent("1900-01-01");
   },
 };
 
