b25bd05bb8715f9371f9d609a040901112b3c84d
presentY1897 <presenty1897@gmail.com>
2025-03-26 12:49:06 +0900

    feat: add korean locale

--- components/organisms/sign-up-username.tsx
+++ components/organisms/sign-up-username.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import AnimatePlaceholderInput from "../atoms/animate-placeholder-input";
 import Button from "../atoms/button";
+import { useTranslations } from "next-intl";
 
 export default function SignUpUsername() {
+  const translate = useTranslations("SignUp");
   const [username, setUsername] = React.useState("");
 
   const confirmUsername = () => {
@@ -14,14 +16,14 @@ export default function SignUpUsername() {
       <div>
         <AnimatePlaceholderInput
           type="text"
-          placeholder="Username"
+          placeholder={translate("Username")}
           inputValue={username}
           inputValueChange={(value) => setUsername(value)}
         />
       </div>
       <div className="min-h-10"></div>
       <div className="place-self-end">
-        <Button text="Next" onClick={() => confirmUsername()} />
+        <Button text={translate("Next")} onClick={() => confirmUsername()} />
       </div>
     </div>
   );
