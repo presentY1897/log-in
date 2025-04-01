8252d2f308ee8090d7cc2c729dc7bd458f410d9c
presentY1897 <presenty1897@gmail.com>
2025-04-01 21:40:20 +0900

    chore: function order

--- app/actions/auth.ts
+++ app/actions/auth.ts
@@ -3,41 +3,6 @@ import { createSession, validateCredentials, insertUser, deleteSession } from "@
 import { redirect } from "next/navigation";
 
 
-export async function logIn(formData: FormData){
-
-	const validatedFields = SignupFormSchema.safeParse({
-		name: formData.get('name'),
-		email: formData.get('email'),
-		password: formData.get('password'),
-	})
-
-	if (!validatedFields.success) {
-		return { 
-			errors: validatedFields.error.flatten().fieldErrors,
-		 }
-	}
-
-	const {name, email, password} = validatedFields.data;
-
-	const { user, error } = await validateCredentials(name, email, password);
-
-	if (error) {
-		return {
-			message: error.message,
-		}
-	}
-
-  if (!user) {
-    return {
-      message: 'An error occurred while sign in your account.',
-    }
-  }
-
-	await createSession(user.id.toString());
-
-	redirect('/');
-}
-
 export async function signUp(formData: FormData){
 
 	const validatedFields = SignupFormSchema.safeParse({
@@ -72,6 +37,41 @@ export async function signUp(formData: FormData){
 
 	redirect('/');
 
+}
+
+export async function logIn(formData: FormData){
+
+	const validatedFields = SignupFormSchema.safeParse({
+		name: formData.get('name'),
+		email: formData.get('email'),
+		password: formData.get('password'),
+	})
+
+	if (!validatedFields.success) {
+		return { 
+			errors: validatedFields.error.flatten().fieldErrors,
+		 }
+	}
+
+	const {name, email, password} = validatedFields.data;
+
+	const { user, error } = await validateCredentials(name, email, password);
+
+	if (error) {
+		return {
+			message: error.message,
+		}
+	}
+
+  if (!user) {
+    return {
+      message: 'An error occurred while sign in your account.',
+    }
+  }
+
+	await createSession(user.id.toString());
+
+	redirect('/');
 }
 
 export async function logOut() {
