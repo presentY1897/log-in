04e6f9b8ec80a94f62706ac0b88aa7efd1d0f450
presentY1897 <presenty1897@gmail.com>
2025-04-01 19:15:19 +0900

    chore: formatter

--- components/atoms/label-input.tsx
+++ components/atoms/label-input.tsx
@@ -1,9 +1,20 @@
-
-export default function LabelInput({ label, type, placeholder }: { label: string, type: string, placeholder: string }) {
-	return (
-		<div className="flex flex-col">
-			<label className="text-primary">{label}</label>
-			<input className="bg-tertiary placeholder-secondary rounded-md" type={type} placeholder={placeholder} />
-		</div>
-	)
+export default function LabelInput({
+  label,
+  type,
+  placeholder,
+}: {
+  label: string;
+  type: string;
+  placeholder: string;
+}) {
+  return (
+    <div className="flex flex-col">
+      <label className="text-primary">{label}</label>
+      <input
+        className="bg-tertiary placeholder-secondary rounded-md"
+        type={type}
+        placeholder={placeholder}
+      />
+    </div>
+  );
 }
