92cab77886555fbb4bfebc3a9dce5d9fa964b9db
presentY1897 <presenty1897@gmail.com>
2025-04-01 17:01:19 +0900

    feat: add password playwright test

--- tests/log-in.spec.ts
+++ tests/log-in.spec.ts
@@ -1,20 +1,65 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
+test("handle log in username", async ({ page }) => {
+  await page.goto("/en/log-in/username");
 
-test('handle log in username', async ({ page })=> {
-	await page.goto('/en/log-in/username');
+  const usernameInput = page.getByTestId("username-input");
+  await usernameInput.fill("testuser");
+  expect(usernameInput).toHaveValue("testuser");
 
-	const usernameInput = page.getByTestId('username-input');
-	await usernameInput.fill('testuser');
-	expect(usernameInput).toHaveValue('testuser');
+  const submitButton = page.getByTestId("username-submit-button");
+  await submitButton.click();
+  await expect(page).toHaveURL("/en/log-in/password");
 
-	const submitButton = page.getByTestId('username-submit-button');
-	await submitButton.click();
-	await expect(page).toHaveURL('/en/log-in/password');
+  const username = await page.evaluate(
+    () => localStorage.getItem("username") || ""
+  );
+  const email = await page.evaluate(() => localStorage.getItem("email") || "");
 
-	const username = await page.evaluate(() => 		localStorage.getItem('username') || '');
-	const email = await page.evaluate(() =>localStorage.getItem('email') || '');
+  expect(username).toBe("testuser");
+  expect(email).toBe("testuser@gmail.com");
+});
 
-	expect(username).toBe('testuser');
-	expect(email).toBe('testuser@gmail.com');
+test("handle log in password page: username fail", async ({ page }) => {
+  await page.goto("/en/log-in/password");
+
+  const passwordInput = page.getByTestId("password-input");
+  await passwordInput.fill("testpassword");
+  await page.evaluate(() => localStorage.setItem("username", "#"));
+
+  const submitButton = page.getByTestId("password-submit-button");
+  await submitButton.click();
+  await expect(page).toHaveURL("/en/log-in/username");
+});
+
+test("handle log in password page: email fail", async ({ page }) => {
+  await page.goto("/en/log-in/password");
+
+  const passwordInput = page.getByTestId("password-input");
+  await passwordInput.fill("testpassword");
+  await page.evaluate(() => localStorage.setItem("email", "aa"));
+
+  const submitButton = page.getByTestId("password-submit-button");
+  await submitButton.click();
+  await expect(page).toHaveURL("/en/log-in/username");
+});
+
+test("handle log in password page: password fail", async ({ page }) => {
+  await page.goto("/en/log-in/password");
+
+  const passwordInput = page.getByTestId("password-input");
+  await passwordInput.fill("testpassword");
+  await page.evaluate(() => localStorage.setItem("username", "test"));
+  await page.evaluate(() => localStorage.setItem("email", "test@test.com"));
+
+  const submitButton = page.getByTestId("password-submit-button");
+  await submitButton.click();
+
+  const passwordError = page.getByTestId("password-error");
+  await expect(passwordError).toBeVisible();
+
+  await passwordInput.clear();
+  await passwordInput.fill("testpassword1@");
+  await submitButton.click();
+  await expect(page).toHaveURL("/en");
 });
