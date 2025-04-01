c5d8998124db5fa78b3604167e41de48574c9d04
presentY1897 <presenty1897@gmail.com>
2025-04-01 12:25:19 +0900

    feat: create sign up password

---
+++ components/organisms/sign-up-password.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import AnimatePlaceholderInput from "../atoms/animate-placeholder-input";
+import Button from "../atoms/button";
+import { useTranslations } from "next-intl";
+
+export default function SignUpPassword() {
+  const translate = useTranslations("SignUp");
+  const [password, setPassword] = React.useState("");
+
+  const confirmPasswordMatch = () => {
+    console.log("check with password", password);
+  };
+
+  return (
+    <div className="flex flex-col gap-2">
+      <div>
+        <AnimatePlaceholderInput
+          type="password"
+          placeholder={translate("Password")}
+          inputValue={password}
+          inputValueChange={(value) => setPassword(value)}
+        />
+      </div>
+      <div className="min-h-10"></div>
+      <div className="place-self-end">
+        <Button
+          text={translate("Submit")}
+          onClick={() => confirmPasswordMatch()}
+        />
+      </div>
+    </div>
+  );
+}
