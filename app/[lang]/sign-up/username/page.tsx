0d11138afbd613a8e2f280244885aee3c3c6300a
presentY1897 <presenty1897@gmail.com>
2025-04-01 21:25:06 +0900

    feat: sign up pages

---
+++ app/[lang]/sign-up/username/page.tsx
@@ -0,0 +1,14 @@
+import { useTranslations } from "next-intl";
+import SignUpUsername from "@/components/organisms/sign-up-username";
+import SingleContentTemplate from "@/components/templates/single-content-template";
+
+export default function Page() {
+  const translate = useTranslations("SignUp");
+  const innerContext = <SignUpUsername />;
+  return (
+    <SingleContentTemplate
+      titleText={translate("Title")}
+      innerContent={innerContext}
+    ></SingleContentTemplate>
+  );
+}
