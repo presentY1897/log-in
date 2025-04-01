44a725e770c8593d33c58443b77a18cd03d282c7
presentY1897 <presenty1897@gmail.com>
2025-04-01 20:42:44 +0900

    chore: formatter

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,34 +1,32 @@
 import { routing } from "@/i18n/routing";
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 import { getMessages } from "next-intl/server";
 import { NextIntlClientProvider } from "next-intl";
 import "@/app/globals.css";
 import { ThemeProvider } from "next-themes";
 
 export default async function RootLayout({
-	children,
-	params,
+  children,
+  params,
 }: Readonly<{
-	children: React.ReactNode;
-	params: Promise<{ lang: string }>;
+  children: React.ReactNode;
+  params: Promise<{ lang: string }>;
 }>) {
-	if (!routing.locales.includes((await params).lang as never)) {
-		notFound();
-	}
+  if (!routing.locales.includes((await params).lang as never)) {
+    notFound();
+  }
 
-	const messages = await getMessages();
+  const messages = await getMessages();
 
-	return (
-		<html lang={(await params).lang} suppressHydrationWarning>
-			<body
-				className={`antialiased`}
-			>
-				<ThemeProvider attribute="class">
-					<NextIntlClientProvider messages={messages}>
-						{children}
-					</NextIntlClientProvider>
-				</ThemeProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang={(await params).lang} suppressHydrationWarning>
+      <body className={`antialiased`}>
+        <ThemeProvider attribute="class">
+          <NextIntlClientProvider messages={messages}>
+            {children}
+          </NextIntlClientProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
