be66e7f39dcc7cd79efd99a91f5b03d63f43befe
presentY1897 <presenty1897@gmail.com>
2025-04-01 13:03:05 +0900

    fix: button props extends html button element

--- components/atoms/button.tsx
+++ components/atoms/button.tsx
@@ -1,36 +1,17 @@
-interface ButtonProps {
+interface ButtonProps extends React.InputHTMLAttributes<HTMLButtonElement> {
+  type?: "button" | "submit" | "reset";
   text: string;
   width?: string;
   height?: string;
-  onClick?: React.MouseEventHandler<HTMLButtonElement>;
-  onMouseDown?: React.MouseEventHandler<HTMLButtonElement>;
-  onMouseUp?: React.MouseEventHandler<HTMLButtonElement>;
-  onKeyDown?: React.KeyboardEventHandler<HTMLButtonElement>;
-  onKeyUp?: React.KeyboardEventHandler<HTMLButtonElement>;
-  disabled?: boolean;
 }
 
-export default function Button({
-  text,
-  width,
-  height,
-  onClick,
-  onMouseDown,
-  onMouseUp,
-  onKeyDown,
-  onKeyUp,
-  disabled = false,
-}: ButtonProps) {
+export default function Button(props: ButtonProps) {
+  const { text, width, height } = props;
   return (
     <button
+      {...props}
       className="bg-foreground text-background rounded-lg border-2 border-foreground px-2 py-1 hover:bg-background hover:text-foreground hover:border-foreground"
       style={{ width: width, height: height }}
-      onClick={onClick}
-      onMouseDown={onMouseDown}
-      onMouseUp={onMouseUp}
-      onKeyDown={onKeyDown}
-      onKeyUp={onKeyUp}
-      disabled={disabled}
     >
       {text}
     </button>
