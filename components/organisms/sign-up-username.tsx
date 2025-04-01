e75a02e5911cb51e203dd52d417f0d45341b05ba
presentY1897 <presenty1897@gmail.com>
2025-04-01 21:25:28 +0900

    feat: check email sign up username

--- components/organisms/sign-up-username.tsx
+++ components/organisms/sign-up-username.tsx
@@ -1,20 +1,44 @@
+"use client";
+
 import React from "react";
 import AnimatePlaceholderInput from "../atoms/animate-placeholder-input";
 import Button from "../atoms/button";
 import { useTranslations } from "next-intl";
+import { EmailUtils } from "@/utils/utils";
+import { checkDuplicateEmail } from "@/app/lib/session";
+import { useRouter } from "next/navigation";
 
 export default function SignUpUsername() {
   const translate = useTranslations("SignUp");
   const [username, setUsername] = React.useState("");
+  const router = useRouter();
 
-  const confirmUsername = () => {
-    console.log("check with username", username);
+  const confirmUsername = async () => {
+    if (username.length === 0) {
+      return false;
+    }
+    const email = EmailUtils.validate.test(username)
+      ? username
+      : username + "@" + EmailUtils.defaultEmail;
+    const { state, error } = await checkDuplicateEmail(email);
+    if (error) {
+      console.error(error.message);
+      return false;
+    }
+    if (state) {
+      console.log("Email already exists");
+      return false;
+    }
+    localStorage.setItem("name", email.split("@")[0]);
+    localStorage.setItem("email", email);
+    router.push("/sign-up/password");
   };
 
   return (
     <div className="flex flex-col gap-2">
       <div>
         <AnimatePlaceholderInput
+          data-testid="username-input"
           type="text"
           placeholder={translate("Username")}
           value={username}
@@ -23,7 +47,11 @@ export default function SignUpUsername() {
       </div>
       <div className="min-h-10"></div>
       <div className="place-self-end">
-        <Button text={translate("Next")} onClick={() => confirmUsername()} />
+        <Button
+          data-testid="username-submit"
+          text={translate("Next")}
+          onClick={() => confirmUsername()}
+        />
       </div>
     </div>
   );
