bebb1dac0fd41e516e3ffd69da58d1b8436dec74
presentY1897 <presenty1897@gmail.com>
2025-04-01 21:38:15 +0900

    feat: add logout button

--- components/organisms/userinfo.tsx
+++ components/organisms/userinfo.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import { format } from "date-fns";
 import LabelInput from "../atoms/label-input";
 import { useTranslations } from "next-intl";
+import Button from "../atoms/button";
+import { logOut } from "@/app/actions/auth";
 
 export interface UserInfoProps {
   username: string;
@@ -17,8 +21,12 @@ export default function UserInfo(props: UserInfoProps) {
     created: format(props.created, "yyyy-MM-dd"),
   };
 
+  const logOutCallback = async () => {
+    await logOut();
+  };
+
   return (
-    <div>
+    <div className="flex flex-col gap-4 min-w-[250px]">
       <LabelInput
         labelText={translate("Username")}
         type="text"
@@ -37,6 +45,13 @@ export default function UserInfo(props: UserInfoProps) {
         disabled={true}
         value={created}
       ></LabelInput>
+      <div className="place-self-end">
+        <Button
+          data-testid="logout-button"
+          text={translate("LogOut")}
+          onClick={() => logOutCallback()}
+        ></Button>
+      </div>
     </div>
   );
 }
