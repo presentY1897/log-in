acba5c37d0deb2d6b2cf2014ce196257fa6f750a
presentY1897 <presenty1897@gmail.com>
2025-04-01 17:38:26 +0900

    feat: add log in func

--- app/actions/auth.ts
+++ app/actions/auth.ts
@@ -1,11 +1,16 @@
-import { SignupFormSchema } from "../lib/definitions";
+import bcrypt from "bcryptjs";
+import { createClient } from "@/utils/supbase/client";
+import { SignupFormSchema } from "@/app/lib/definitions";
+import { createSession } from "@/app/lib/session";
+import { redirect } from "next/navigation";
 
-export async function login(data: FormData){
+
+export async function login(formData: FormData){
 
 	const validatedFields = SignupFormSchema.safeParse({
-		name: data.get('name'),
-		email: data.get('email'),
-		password: data.get('password'),
+		name: formData.get('name'),
+		email: formData.get('email'),
+		password: formData.get('password'),
 	})
 
 	if (!validatedFields.success) {
@@ -13,5 +18,36 @@ export async function login(data: FormData){
 			errors: validatedFields.error.flatten().fieldErrors,
 		 }
 	}
+
+	const {name, email, password} = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+	const supabase = createClient();
+	const { data, error } = await supabase.from('user-info').select('*').eq('username', name).is('email', email).eq('password', hashedPassword).limit(1);
+
+	if (error) {
+		return {
+			message: error.message,
+		}
+	}
+
+	if (data == null || data.length == 0){
+		return {
+			errors: {
+				name: ['Username or email not found'],
+			}
+		}
+	}
+
+	const user = data[0];
+
+  if (!user) {
+    return {
+      message: 'An error occurred while creating your account.',
+    }
+  }
+
+	await createSession(user.id.toString());
+	redirect('/');
 
 }
