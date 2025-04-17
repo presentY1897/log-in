d2384c03030de303ffb0bdad2ade1bd533284526
presentY1897 <presenty1897@gmail.com>
2025-04-17 15:24:33 +0900

    refactor: change Button component forwardRef for responsibility

--- components/atoms/button.tsx
+++ components/atoms/button.tsx
@@ -1,19 +1,30 @@
+import { cn } from "@/utils/tailwind";
+import React from "react";
+
 interface ButtonProps extends React.InputHTMLAttributes<HTMLButtonElement> {
   type?: "button" | "submit" | "reset";
   width?: string;
   height?: string;
-  // className?: string; how to appropriately extend the className prop?
 }
 
-export default function Button(props: ButtonProps) {
-  const { width, height, children, ...attributes } = props;
-  return (
-    <button
-      {...attributes}
-      className="bg-foreground text-background rounded-lg border-2 border-foreground px-2 py-1 hover:bg-background hover:text-foreground hover:border-foreground"
-      style={{ width: width, height: height }}
-    >
-      {children}
-    </button>
-  );
-}
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, width, height, children, ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        style={{ width, height }}
+        {...props}
+        className={cn(
+          "bg-foreground text-background rounded-lg border-2 border-foreground px-2 py-1 hover:bg-background hover:text-foreground hover:border-foreground",
+          className
+        )}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+Button.displayName = "Button";
+
+export default Button;
